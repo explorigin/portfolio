9b454c1ece2485c827c3c60e6c00db7ccc6f3006
Timothy Farrell <tim@thecookiejar.me>
2017-09-29 22:44:37 -0500

    Remove unnecessary ThumbnailView.js

--- packages/gallery/src/interface/image.js
+++ packages/gallery/src/interface/image.js
@@ -1,5 +1,4 @@
 import { defineView, defineElement as el } from 'domvm';
-import { ThumbnailView } from './thumbnail.js';
 
 
 export function ImageView(vm, model) {
@@ -12,26 +11,48 @@ export function ImageView(vm, model) {
     return function(vm, model, key, opts) {
         const { imageRow, showTags, remove, addTag, removeTag } = model;
         const { doc } = imageRow;
+        const { _id: id, _rev: rev, tags } = doc;
         const { thumbnail } = doc._attachments;
         const _showTags = showTags !== undefined ? showTags : true;
+        const filteredTags = (
+            _showTags
+            ? Object.entries(doc.tags).filter(([_, visible]) => visible)
+            : []
+        );
 
         if (thumbnail) {
             return el('div', [
-                defineView(ThumbnailView, {
-                    id: doc._id,
-                    rev: doc._rev,
-                    name: 'thumbnail',
-                    doc: thumbnail,
-                    tags: _showTags ? doc.tags : [],
-                    remove: remove,
-                    removeTag: removeTag
-                }),
+                el(
+                    `figure#${doc._id}.image`,
+                    [
+                        el('img',
+                            {
+                                src: `data:${thumbnail.content_type};base64,${thumbnail.data}`,
+                                title: `${id} ${name}`,
+                                "data-id": id,
+                                onclick: [remove, id, rev]
+                            }
+                        ),
+                        (
+                            filteredTags.length
+                            ? (
+                                el('figcaption',
+                                    filteredTags.map(([title, _]) => el(
+                                        'span.tag',
+                                        { onclick: [removeTag, title, id] },
+                                        [ title ]
+                                    ))
+                                )
+                            )
+                            : null
+                        )
+                    ]
+                ),
                 (
                     addTag
-                    ? el('button', { onclick: [onAddTag, doc._id] }, '+')
+                    ? el('button', { onclick: [onAddTag, id] }, '+')
                     : null
                 )
-
             ]);
         }
 
