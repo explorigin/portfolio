0fe99ec2f6531eaf59ca30a1a21e08f2e7656e6f
Timothy Farrell <tim@thecookiejar.me>
2017-11-03 07:23:33 -0500

    Use the latest FRPtools

--- packages/gallery/src/utils/comparators.js
+++ packages/gallery/src/utils/comparators.js
@@ -3,6 +3,9 @@ import { equals } from './set.js';
 
 
 export function pouchDocArrayComparator(a, b) {
+    if (!Array.isArray(b)) {
+        return false;
+    }
     const aIDs = a.map(extractID);
     const bIDs = b.map(extractID);
 
