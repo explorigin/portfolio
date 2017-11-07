9608ed179cc19a96aed425d6eb78c2cbb7bfd374
Timothy Farrell <tim@thecookiejar.me>
2017-11-06 20:38:27 -0600

    Indentation changes only.

--- packages/gallery/webpack.config.js
+++ packages/gallery/webpack.config.js
@@ -2,22 +2,22 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
-  entry: {
-    app: './app.js',
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
-    publicPath: '/assets',
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './src'),
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __DEV__: true,
-    })
-  ],
-  devtool: 'source-map',
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        app: './app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+        publicPath: '/assets',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './src'),
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __DEV__: true,
+        })
+    ],
+    devtool: 'source-map',
 };
